3955d506c7f10174cdff0e720d9c23198478bb62
Eric Nyirimana <ericnyirimana31@gmail.com>
2019-01-09 00:02:53 +0200

    Questions Model queries

--- queries/questions.js
+++ queries/questions.js
@@ -13,8 +13,22 @@ function addQuestion(data) {
         if (err) throw err;
     });
 }
+// Downvote Question
+function downvotes(data) {
+    fs.writeFile('./data/questions.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) throw err;
+    });
+}
+// upvote Question
+function upvotes(data) {
+    fs.writeFile('./data/questions.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) throw err;
+    });
+}
 module.exports = {
     addQuestion,
+    downvotes,
+    upvotes,
     questions: questionsList
 
 };
